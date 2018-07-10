6915843481309f47eef2619db45c244c1021b737
Arrizky Dwinanda Ramadhan <39751877+arrizky@users.noreply.github.com>
2018-07-10 22:07:15 +0700

    Update index.php

--- CodeIgniter/application/views/index.php
+++ CodeIgniter/application/views/index.php
@@ -519,57 +519,36 @@
 			<div class="slick1">
 				<div class="item-slick1 item1-slick1" style="background-image: url(<?php echo base_url('assets/images/bg1.jpg');?>);">
 					<div class="wrap-content-slide1 sizefull flex-col-c-m p-l-15 p-r-15 p-t-150 p-b-170">
-						<span class="caption1-slide1 m-text1 t-center animated visible-false m-b-15" data-appear="fadeInDown">
-							Handphone
-						</span>
-
 						<h2 class="caption2-slide1 xl-text1 t-center animated visible-false m-b-37" data-appear="fadeInUp">
-							New arrivals
+							Handphone
 						</h2>
-
 						<div class="wrap-btn-slide1 w-size1 animated visible-false" data-appear="zoomIn">
 							<!-- Button -->
-							<a href="product.html" class="flex-c-m size2 bo-rad-23 s-text2 bgwhite hov1 trans-0-4">
-								Shop Now
-							</a>
+							
 						</div>
 					</div>
 				</div>
 
 				<div class="item-slick1 item2-slick1" style="background-image: url(<?php echo base_url('assets/images/bg2.jpg');?>);">
 					<div class="wrap-content-slide1 sizefull flex-col-c-m p-l-15 p-r-15 p-t-150 p-b-170">
-						<span class="caption1-slide1 m-text1 t-center animated visible-false m-b-15" data-appear="rollIn">
-							Laptop
-						</span>
-
 						<h2 class="caption2-slide1 xl-text1 t-center animated visible-false m-b-37" data-appear="lightSpeedIn">
-							New arrivals
+							Laptop
 						</h2>
 
 						<div class="wrap-btn-slide1 w-size1 animated visible-false" data-appear="slideInUp">
 							<!-- Button -->
-							<a href="product.html" class="flex-c-m size2 bo-rad-23 s-text2 bgwhite hov1 trans-0-4">
-								Shop Now
-							</a>
 						</div>
 					</div>
 				</div>
 
 				<div class="item-slick1 item3-slick1" style="background-image: url(<?php echo base_url('assets/images/bg3.jpg');?>);">
 					<div class="wrap-content-slide1 sizefull flex-col-c-m p-l-15 p-r-15 p-t-150 p-b-170">
-						<span class="caption1-slide1 m-text1 t-center animated visible-false m-b-15" data-appear="rotateInDownLeft">
-							Refrigerator
-						</span>
-
 						<h2 class="caption2-slide1 xl-text1 t-center animated visible-false m-b-37" data-appear="rotateInUpRight">
-							New arrivals
+							Refrigerator
 						</h2>
 
 						<div class="wrap-btn-slide1 w-size1 animated visible-false" data-appear="rotateIn">
 							<!-- Button -->
-							<a href="product.html" class="flex-c-m size2 bo-rad-23 s-text2 bgwhite hov1 trans-0-4">
-								Shop Now
-							</a>
 						</div>
 					</div>
 				</div>
